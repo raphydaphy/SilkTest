527e5a60e75af90c3ac563c97c0a7a7a87cb546e
Raph Hennessy <raph.hennessy@gmail.com>
2019-03-19 11:01:37 +1100

    its not silk

--- src/main/java/com/raphydaphy/silktest/SilkTest.java
+++ src/main/java/com/raphydaphy/silktest/SilkTest.java
@@ -1,9 +1,7 @@
 package com.raphydaphy.silktest;
 
-import io.github.prospector.silk.fluid.FluidInstance;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.CommandRegistry;
-import net.minecraft.fluid.Fluids;
 import net.minecraft.server.command.ServerCommandManager;
 import net.minecraft.text.StringTextComponent;
 
@@ -15,7 +13,6 @@ public class SilkTest implements ModInitializer
 	{
 		CommandRegistry.INSTANCE.register(false, dispatcher -> dispatcher.register(ServerCommandManager.literal("silk-test").executes(command ->
 		{
-			FluidInstance i = new FluidInstance(Fluids.EMPTY);
 			command.getSource().sendFeedback(new StringTextComponent("It worked!"), false);
 			return 1;
 		})));
